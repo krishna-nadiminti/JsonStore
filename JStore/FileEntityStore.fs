af718f6b5427be9366bff4d9ab988f494ee765c9
Karthik Padmanabhan {PK} <karthi.email@gmail.com>
2014-10-07 23:24:26 +1100

    refactoring c# code to simplify porting

--- JStore/FileEntityStore.fs
+++ JStore/FileEntityStore.fs
@@ -2,6 +2,7 @@
 
 open System
 open System.Collections.Generic
+open System.Linq
 open PCLStorage
 
 [<Struct>]
@@ -12,6 +13,6 @@ type public EntityTypeInfo (``type``: Type) =
         with get() = "Money.Model." + x.Type.Name + ".dat"
 
 
-type public FileEntityStore = class end
+//type public FileEntityStore =
 //    static member CreateFileStore(folder: IFolder, entitytype: EntityTypeInfo) = 
-//        store = Activator.CreateInstance()
+//        let entitySetType = typedefof(EntitySet<_>).MakeGericType(entityType.Type)
