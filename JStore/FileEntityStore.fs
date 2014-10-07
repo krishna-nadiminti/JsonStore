9a01e08e2141d8778b069e632b3c965ce8e7d2b5
Karthik Padmanabhan {PK} <karthi.email@gmail.com>
2014-10-07 12:15:08 +1100

    first tye in f#

---
+++ JStore/FileEntityStore.fs
@@ -0,0 +1,14 @@
+﻿namespace JStore
+
+open System
+
+[<Struct>]
+type EntityTypeInfo (``type``: Type) =
+    member x.``Type``
+        with get() = ``type``
+    member x.Filename
+        with get() = "Money.Model." + x.Type.Name + ".dat"
+
+
+type FileEntityStore() = 
+    member this.X = "F#"
